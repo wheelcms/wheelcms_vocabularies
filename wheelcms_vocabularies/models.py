d0095e80476cd50927c16a1ac9677585bea2d171
Ivo van der Wijk <github@in.m3r.nl>
2014-02-13 18:44:38 +0100

    Implement handling of post

--- wheelcms_vocabularies/models.py
+++ wheelcms_vocabularies/models.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+
 from wheelcms_axle.models import Configuration as BaseConfiguration
 from wheelcms_axle.configuration import BaseConfigurationHandler
 
@@ -5,13 +7,14 @@ from wheelcms_axle.registries.configuration import configuration_registry
 from django.db import models
 from django import forms
 
-class Vocabulary(models.Model):
-    key = models.CharField(max_length=60, blank=False)
-    raw = models.TextField(default="[]", blank=False)
 
 class Configuration(models.Model):
     main = models.ForeignKey(BaseConfiguration, related_name="vocabularies")
-    vocabs = models.ManyToManyField(Vocabulary, related_name="configuration")
+
+class Vocabulary(models.Model):
+    conf = models.ForeignKey(Configuration, related_name="vocabularies")
+    key = models.CharField(max_length=60, blank=False)
+    raw = models.TextField(default="[]", blank=False)
 
 class ConfigurationForm(forms.ModelForm):
     class Meta:
@@ -28,6 +31,27 @@ class ConfigurationHandler(BaseConfigurationHandler):
         handler.context['tabs'] = handler.construct_tabs(self.id)
         ## set redirect_to
         return handler.template("wheelcms_vocabularies/configure_vocabularies.html")
+
+    def process(self, handler, instance):
+        handler.context['form'] = form = \
+                 self.form(handler.request.POST, instance=instance)
+        
+        if form.is_valid():
+            form.save()
+            for d in handler.request.POST.getlist('delete', []):
+                try:
+                    Vocabulary.objects.get(key=d).delete()
+                except Vocabulary.DoesNotExist:
+                    pass
+            for i, k in enumerate(handler.request.POST.getlist('voc.id', [])):
+                v, _ = Vocabulary.objects.get_or_create(key=k, conf=instance)
+                v.data = handler.request.POST.get('voc.data')[i]
+
+            ## include hash, open tab
+            return handler.redirect(reverse('wheel_config'), config=self.id, success="Changes saved")
+        handler.context['tabs'] = handler.construct_tabs(self.id)
+        return handler.template("wheelcms_vocabularies/configure_vocabularies.html")
+
 #configuration_registry.register("vocabularies", "Vocabularies", Configuration, ConfigurationForm)
 configuration_registry.register(ConfigurationHandler)
 
--- wheelcms_vocabularies/static/js/config_vocabularies.js
+++ wheelcms_vocabularies/static/js/config_vocabularies.js
@@ -1,1 +1,25 @@
-/* */
+app.controller('VocabCtrl', function($scope) {
+    $scope.newvocabulary = {};
+    $scope.deleted = [];
+
+    $scope.vocabularies = [
+        {identifier:"foo.bar",
+         data:"{bla bla bla}"},
+        {identifier:"arg.bar",
+         data:"{bla flop bla}"},
+        {identifier:"frap.flop.pip",
+         data:"{asdfasdf asdfsd sdasdf}"}
+
+    ];
+
+    $scope.addVocabulary = function() {
+        $scope.vocabularies.push($scope.newvocabulary);
+        $scope.newvocabulary = {};
+    };
+
+    $scope.removeVocabulary = function(id) {
+        $scope.deleted.push($scope.vocabularies[id].identifier);
+        $scope.vocabularies.splice(id, 1);
+        console.log($scope.deleted);
+    };
+});
